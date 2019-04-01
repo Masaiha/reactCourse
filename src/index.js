08616366a404d505f2b924a63086d0fce27ef462
Masanori Iha <masanori@engage.bz>
2019-04-01 20:46:25 -0300

    aula 12 -  Olá Mundo

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 
+const App = () =>{
+	return <div> Olá mundo! </div>;
+}
 
+
+ReactDom.render(<App />, document.querySelector("#root"))
