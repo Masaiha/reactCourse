300b068a42d0fd0c6574e9da280e4a13a0e9322d
Masanori Iha <masanori@engage.bz>
2019-04-01 21:47:26 -0300

    aula 20

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 
+function buttonText(){
+	return "Click me!";
+}
+
 const App = () =>{
-	return <div> Olá mundo! </div>;
+	return (
+		<div>
+			<label className="label" for="name">
+				Enter Name:
+			</label>
+			<input id="name" type="text" />
+			<button style={{ backgroundColor: 'blue', color: 'white' }}>
+				{ buttonText() }
+			</button>
+		</div>
+
+	);
 }
 
 
