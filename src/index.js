4ff2c1fbb4b599214207e85a79c52ddb457f417a
Masanori Iha <masanori@engage.bz>
2019-04-01 21:57:41 -0300

    aula 21

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-
-function buttonText(){
-	return "Click me!";
-}
-
+// create a rect component
 const App = () =>{
+
+	//const buttonText = "Click me!"
+	const buttonText = { text: "Click me!" }
+
 	return (
 		<div>
 			<label className="label" for="name">
@@ -14,7 +14,7 @@ const App = () =>{
 			</label>
 			<input id="name" type="text" />
 			<button style={{ backgroundColor: 'blue', color: 'white' }}>
-				{ buttonText() }
+				{ buttonText.text }
 			</button>
 		</div>
 
